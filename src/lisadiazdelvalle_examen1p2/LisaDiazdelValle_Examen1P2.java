5a4c3262e094aff324200bb22b260dffd27ab70e
Lisa Diaz del Valle <lmdvl04@gmail.com>
2023-02-17 13:23:20 -0600

    principal

---
+++ src/lisadiazdelvalle_examen1p2/LisaDiazdelValle_Examen1P2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lisadiazdelvalle_examen1p2;
+
+/**
+ *
+ * @author HTS
+ */
+public class LisaDiazdelValle_Examen1P2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
