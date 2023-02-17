a22f1709085cc10aded765f4d539c76c75502a6f
Lisa Diaz del Valle <lmdvl04@gmail.com>
2023-02-17 15:53:22 -0600

    Creacion de gui agregar

--- src/lisadiazdelvalle_examen1p2/LisaDiazdelValle_Examen1P2.java
+++ src/lisadiazdelvalle_examen1p2/LisaDiazdelValle_Examen1P2.java
@@ -14,7 +14,8 @@ public class LisaDiazdelValle_Examen1P2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        GUI G = new GUI();
+        G.setVisible(true);
     }
     
 }
